27352b8c362b7ade5ce83bbdee2ebbc05616c535
David Kaufmann <david@bit-dat.com>
2024-10-31 15:54:36 +0100

    first try to create network. Missing information about architecture for MNIST

--- data_loader.py
+++ data_loader.py
@@ -1,22 +1,27 @@
 import torch 
 from torch.utils.data import Dataset
-from torchvision import datasets
-from torchvision.transforms import ToTensor
+from torchvision import datasets, transforms
 
 
 def load_MNIST():
+  # TODO discuss this because this does not quite fit how they describe it in the paper
+  transform = transforms.Compose([
+    transforms.Pad(padding=2),
+    transforms.ToTensor()
+  ])
+
   training_data = datasets.MNIST(
     root='data',
     train=True,
     download=True,
-    transform=ToTensor()
+    transform=transform
   )
 
   test_data = datasets.MNIST(
     root='data',
     train=False,
     download=True,
-    transform=ToTensor()
+    transform=transform
   )
 
   return training_data, test_data
---
+++ idem_net.py
@@ -0,0 +1,84 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+class Encoder(nn.Module):
+  def __init__(self, in_channels=1, relu_slope=0.2):
+    super(Encoder, self).__init__()
+
+    self.relu_slope = relu_slope
+
+    self.conv1 = nn.Conv2d(in_channels, out_channels=64, kernel_size=4, stride=2, padding=1)  # need to set in_channels to 3 for color images
+
+    self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=4, stride=2, padding=1)
+    self.bn2 = nn.BatchNorm2d(128)
+
+    self.conv3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=4, stride=2, padding=1)
+    self.bn3 = nn.BatchNorm2d(256)
+
+    self.conv4 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=4, stride=2, padding=1)
+    self.bn4 = nn.BatchNorm2d(512)
+
+    self.conv5 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=4, stride=1, padding=0)
+
+  def forward(self, x):
+    x = F.leaky_relu(self.conv1(x), self.relu_slope)
+
+    x = F.leaky_relu(self.bn2(self.conv2(x)), self.relu_slope)
+
+    x = F.leaky_relu(self.bn3(self.conv3(x)), self.relu_slope)
+
+    x = F.leaky_relu(self.bn4(self.conv4(x)), self.relu_slope)
+
+    x = self.conv5(x)
+
+    return x
+
+class Decoder(nn.Module):
+  def __init__(self, out_channels=1):
+    super(Decoder, self).__init__()
+
+    self.tconv1 = nn.ConvTranspose2d(in_channels=512, out_channels=512, kernel_size=4, stride=1, padding=0)
+    self.bn1 = nn.BatchNorm2d(512)
+
+    self.tconv2 = nn.ConvTranspose2d(in_channels=512, out_channels=256, kernel_size=4, stride=2, padding=1)
+    self.bn2 = nn.BatchNorm2d(256)
+
+    self.tconv3 = nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=4, stride=2, padding=1)
+    self.bn3 = nn.BatchNorm2d(128)
+
+    self.tconv4 = nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=4, stride=2, padding=1)
+    self.bn4 = nn.BatchNorm2d(64)
+
+    self.tconv5 = nn.ConvTranspose2d(in_channels=64, out_channels=out_channels, kernel_size=4, stride=2, padding=1)
+
+  def forward(self, x):
+    x = F.relu(self.bn1(self.tconv1(x)))
+
+    x = F.relu(self.bn2(self.tconv2(x)))
+
+    x = F.relu(self.bn3(self.tconv3(x)))
+
+    x = F.relu(self.bn4(self.tconv4(x)))
+
+    x = F.tanh(self.tconv5(x))
+
+    return x
+
+
+
+class IdemNet (nn.Module):
+  def __init__(self) -> None:
+    super(IdemNet, self).__init__()
+
+    # define layers
+    self.encoder = Encoder()
+
+    self.decoder = Decoder()
+
+  def forward(self, x):
+    x = self.encoder(x)
+    
+    x = self.decoder(x)
+
+    return x
